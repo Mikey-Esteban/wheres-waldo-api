fa0d923ee86c0651d4c049d5472a1191afafd664
Mikey <michaelp.esteban@gmail.com>
2021-12-27 15:08:09 -0800

    added netlify link to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'
+    origins 'http://localhost:3001, https://wheres-waldo-mikey-esteban.netlify.app/'
 
     resource '*',
       headers: :any,
